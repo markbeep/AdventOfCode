66133dd6381ed8dac6c4ea69d794cfd283b07791
Mark Csurgay <mcsurgay@student.ethz.ch>
2021-12-03 06:41:11 +0100

    day 3 messy

--- 2021 (JS)/day3.js
+++ 2021 (JS)/day3.js
@@ -9,5 +9,48 @@ function nu(a) {
 
 let d = fs.readFileSync("day3.txt", "utf8");
 d = tr(d);
-d = nu(d);
 console.log(d);
+
+let gamma = "";
+let eps = "";
+let ne = d;
+let tmp;
+
+for (let i = 0; i < d[0].length; i++) {
+    let k = ne.reduce((tot, a) => Number(tot)+Number(a[i]), 0);
+    let b;
+    if (k >= ne.length / 2) {
+        b = "1";
+    } else {
+        b = "0";
+    }
+    if (ne.length <= 1) break;
+    tmp = [];
+    ne.forEach(element => {
+        if (b == element[i]) tmp.push(element);
+    });
+    ne = tmp;
+}
+gamma = ne[0];
+
+ne = d;
+tmp = [];
+
+for (let i = 0; i < d[0].length; i++) {
+    let k = ne.reduce((tot, a) => Number(tot)+Number(a[i]), 0);
+    let b;
+    if (k >= ne.length / 2) {
+        b = 0;
+    } else {
+        b = 1;
+    }
+    if (ne.length <= 1) break;
+    tmp = [];
+    ne.forEach(element => {
+        if (b == element[i]) tmp.push(element);
+    });
+    ne = tmp;
+}
+eps = ne[0];
+
+console.log(parseInt(gamma, 2) * parseInt(eps, 2));
