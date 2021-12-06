68b24580ae826d01bf6992d576c544d393fbd882
Mark Csurgay <mcsurgay@student.ethz.ch>
2021-12-06 11:36:25 +0100

    257 chars

--- 2021 (JS)/day6.js
+++ 2021 (JS)/day6.js
@@ -3,12 +3,10 @@ let fs = require('fs');
 let d = fs.readFileSync("day6.txt", "utf8")
     .split(",")
     .map(e => e*1);
-
-let count = new Array(9).fill(0);
-d.forEach(e => count[e]++);
-for (let day = 0; day < 256; day++) {
-    count.push(count.shift());
-    count[6] += count[8];
+let x = Array(9).fill(0), y=256; // y is the days
+d.forEach(e => x[e]++);
+while(y-->0){
+    x.push(x.shift());
+    x[6] += x[8];
 }
-console.log(count.reduce((t, a) => t+a, 0));
-
+console.log(x.reduce((t, a) => t+a));
