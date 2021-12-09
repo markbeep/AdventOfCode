7f83f1b904d51f89661d5a44a5508cad39732010
Mark Csurgay <mcsurgay@student.ethz.ch>
2021-12-09 09:30:54 +0100

    visual stuff

--- 2021 (JS)/day9.js
+++ 2021 (JS)/day9.js
@@ -29,11 +29,32 @@ console.log("Task 1: " + t);
 let k = [].concat(...c).filter(e => e > 0).sort((a, b) => b-a);
 console.log("Task 2: " + k[0]*k[1]*k[2]);
 
+// draws in console
+let bcolors = {
+    HEADER: '\033[95m',
+    OKBLUE: '\033[94m',
+    OKCYAN: '\033[96m',
+    OKGREEN: '\033[2m',
+    WARNING: '\033[2m',
+    FAIL: '\033[91m',
+    ENDC: '\033[0m',
+    BOLD: '\033[1m',
+    UNDERLINE: '\033[4m',
+}
+for (let y = 0; y < dp.length; y++) {
+    let line = "";
+    for (let x = 0; x < dp[0].length; x++) {
+        if (!equal(dp[y][x])) line += "\033[" + (c[dp[y][x][0]][dp[y][x][1]] % 5 + 90) + "m" + "█" + bcolors.ENDC;
+        else line += bcolors.WARNING + "█" + bcolors.ENDC;
+    }
+    console.log(line);
+}
+
 
 // ----------- HELPER FUNCS -----------
 function dfs(y, x) {
     if (d[y][x] === 9 || vis[y][x] !== 0) return dp[y][x];  // ignore 9's or already visited
-    if (isLowest(y, x)) return [y, x];  // lowest point? return coordinates
+    if (isLowest(y, x)) return dp[y][x] = [y, x];  // lowest point? return coordinates
     vis[y][x] = 1;
     let l = [];  // array to store all potential bases
     if (x>0 && d[y][x-1] < d[y][x]) l.push(dfs(y, x-1));
