101f8d061c693f5da0d0fa34f53f97d36e6f540c
Mark Csurgay <mcsurgay@student.ethz.ch>
2021-12-14 09:27:49 +0100

    cleanup

--- 2021 (JS)/day14.js
+++ 2021 (JS)/day14.js
@@ -5,23 +5,15 @@ require('fs').readFile("day14.txt", "utf-8", function(err, data) {
     main(temp, ins);
 });
 
+const DAY = 40;
+
 function main(temp, ins) {
+    const count = (e) => temp.split(e).length-1;  // count occurences of string in string
     let letters = new Array(...new Set([].concat(...ins).join("").split("")));  // all used letters in a list 
     let com = {};
-    letters.forEach(a => letters.forEach(b => com[a+b]=0));
-    for (let i = 0; i < temp.length-1; i++) {
-        com[temp.slice(i, i+2)]++;
-    }
-    let letc = {};
-    for (let i = 0; i < letters.length; i++) {
-        letc[letters[i]] = 0;
-    }
-    for (let i = 0; i < temp.length; i++) {
-        letc[temp[i]]++;
-    }
-    for (let i = 0; i < 40; i++) {
-        com = sim(com, ins, letc);
-    }
+    letters.forEach(a => letters.forEach(b => com[a+b]=count(a+b)));  // init char combination count
+    let letc = letters.reduce((t, e) => ({...t, [e]: count(e)}), {}); // init letter count dict
+    for (let i = 0; i < DAY; i++) com = sim(com, ins, letc);  // repeat simulation DAY times
     let values = Object.values(letc);
     console.log(Math.max(...values) - Math.min(...values));
 }
@@ -29,15 +21,14 @@ function main(temp, ins) {
 function sim(com, ins, letc) {
     let copy = {};
     let keys = Object.keys(com);
-    for (let i = 0; i < keys.length; i++) {
-        let st = keys[i];
-        let res = ins.filter(e => e[0] === st);
+    keys.forEach(st => {
+        let res = ins.filter(e => e[0] === st);  // is valid transition?s
         if (res.length > 0) {
             let [from, to] = res[0];
             copy[from[0]+to] = (copy[from[0]+to] || 0) + com[from];
             copy[to+from[1]] = (copy[to+from[1]] || 0) + com[from];
             letc[to] += com[from];
         }
-    }
+    })
     return copy;
 }
