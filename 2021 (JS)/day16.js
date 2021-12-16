4e9dbcb1c300439f334ccd4114c3b9c882e8f9e9
Mark Csurgay <mcsurgay@student.ethz.ch>
2021-12-16 14:52:25 +0100

    Cleaned up

--- 2021 (JS)/day16.js
+++ 2021 (JS)/day16.js
@@ -11,11 +11,19 @@ function main(binary) {
     console.log("Task 2:", Number(lit.value));
 }
 
-class Literal {
+class Packet {
     constructor(binary) {
         this.version = BigInt("0b"+binary.slice(0, 3));
         this.totalVersion = this.version;
         this.type = BigInt("0b"+binary.slice(3, 6));
+        this.totalPacketSize = 7n;  // per default we assume an OP
+        this.value = 0n;
+    }
+}
+
+class Literal extends Packet {
+    constructor(binary) {
+        super(binary);
         let l = [];
         let cursor = 0n;
         let str = binary.slice(6);
@@ -29,55 +37,34 @@ class Literal {
     }
 }
 
-class Operator {
+class Operator extends Packet {
     constructor(binary) {
-        this.version = BigInt("0b"+binary.slice(0, 3));
-        this.binary = binary;
-        this.type = BigInt("0b"+binary.slice(3, 6));
-        this.totalPacketSize = 7n;
+        super(binary);
         this.totalVersion = this.version;
         this.subpackets = [];
-        this.typeGotten = -1;
-        this.value = 0n;
+        
+        let str, length = Infinity, packetsCount = Infinity;
         let lengthIndicator = parseInt(binary[6]);
-
         if (lengthIndicator == 0) {
             this.totalPacketSize += 15n;
-            if (binary.length < 22) return;
-            let length = BigInt("0b"+binary.slice(7, 22));
-            let str = binary.slice(22);
-            let lengthAdded = 0n;
-            while (str.length > 11 && lengthAdded < length) {
-                let lit;
-                if (getType(str) === 4n) lit = new Literal(str);
-                else lit = new Operator(str);
-                this.typeGotten = getType(str);
-                this.subpackets.push(lit);
-                str = str.slice(Number(lit.totalPacketSize));
-                this.totalPacketSize += lit.totalPacketSize;
-                lengthAdded += lit.totalPacketSize;
-                this.totalVersion += lit.totalVersion;
-            }
+            length = BigInt("0b"+binary.slice(7, 22));
+            str = binary.slice(22);
         } else {
             this.totalPacketSize += 11n;
-            let packetsCount = BigInt("0b"+binary.slice(7, 18));
-            let str = binary.slice(18);
-            for (let i = 0n; i < packetsCount; i++) {
-                let lit;
-                if (str.length < 11) break;
-                if (getType(str) === 4n) lit = new Literal(str);
-                else lit = new Operator(str);
-                this.subpackets.push(lit);
-                str = str.slice(Number(lit.totalPacketSize));
-                this.totalPacketSize += lit.totalPacketSize;
-                this.totalVersion += lit.totalVersion;
-            }
+            packetsCount = parseInt(binary.slice(7, 18), 2);
+            str = binary.slice(18);
         }
+        let res = createPackets(str, length, packetsCount);
+        this.subpackets = res.subpackets;
+        this.totalPacketSize += res.lengthAdded;
+        this.totalVersion += res.totalVersion;
+
+        // handle operation
         switch (this.type) {
-            case 0n: this.value = sum(...this.subpackets); break;
-            case 1n: this.value = prod(...this.subpackets); break;
-            case 2n: this.value = min(...this.subpackets); break;
-            case 3n: this.value = max(...this.subpackets); break;
+            case 0n: this.value = this.subpackets.reduce((t, a) => t+=a.value, 0n); break;
+            case 1n: this.value = this.subpackets.reduce((t, a) => t*=a.value, 1n); break;
+            case 2n: this.value = BigInt(Math.min(...this.subpackets.map(e => Number(e.value)))); break;
+            case 3n: this.value = BigInt(Math.max(...this.subpackets.map(e => Number(e.value)))); break;
             case 5n: this.value = (this.subpackets[0].value > this.subpackets[1].value) ? 1n: 0n; break;
             case 6n: this.value = (this.subpackets[0].value < this.subpackets[1].value) ? 1n: 0n; break;
             case 7n: this.value = (this.subpackets[0].value === this.subpackets[1].value) ? 1n: 0n; break;
@@ -85,36 +72,27 @@ class Operator {
     }
 }
 
-function max() {
-    let s = arguments[0].value;
-    for (let i = 1; i < arguments.length; i++) {
-        s = BigInt(Math.max(Number(arguments[i].value), Number(s)));
+/*
+ * Creates a list of packets for an Operator and additionally
+ * sums up all versions and returns the size of the packet.
+ */
+function createPackets(binary, length, iterations) {
+    let str = binary;
+    let subpackets = [];
+    let lengthAdded = 0n;
+    let totalVersion = 0n;
+    let i = 0;
+    while (lengthAdded < length && i < iterations) {
+        let lit;
+        if (getType(str) === 4n) lit = new Literal(str);
+        else lit = new Operator(str);
+        subpackets.push(lit);
+        str = str.slice(Number(lit.totalPacketSize));
+        lengthAdded += lit.totalPacketSize;
+        totalVersion += lit.totalVersion;
+        i++;
     }
-    return s;
-}
-
-function min() {
-    let s = arguments[0].value;
-    for (let i = 1; i < arguments.length; i++) {
-        s = BigInt(Math.min(Number(arguments[i].value), Number(s)));
-    }
-    return s;
-}
-
-function prod() {
-    let s = 1n;
-    for (let i = 0; i < arguments.length; i++) {
-        s *= arguments[i].value;
-    }
-    return s;
-}
-
-function sum() {
-    let s = 0n;
-    for (let i = 0; i < arguments.length; i++) {
-        s += arguments[i].value;
-    }
-    return s;
+    return { lengthAdded, subpackets, totalVersion }
 }
 
 function getType(binary) {
