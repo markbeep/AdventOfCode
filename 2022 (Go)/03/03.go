35ccf593b9885e9c54631346f5edbeb5ccccca90
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-03 10:53:21 +0100

    faster

--- 2022 (Go)/03/03.go
+++ 2022 (Go)/03/03.go
@@ -12,35 +12,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var c int = 0
 	content := strings.Split(strings.Trim(string(f), " \n"), "\n")
+	a := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	ch := make(chan int, len(content)/3)
 	for i := 0; i < len(content); i += 3 {
-		go calc(content[i], content[i+1], content[i+2], ch)
+		go func(l1, l2, l3 string, ch chan int) {
+			for _, k1 := range l1 {
+				for _, k2 := range l2 {
+					for _, k3 := range l3 {
+						if k1 == k2 && k2 == k3 {
+							ch <- strings.IndexRune(a, k1) + 1
+							return
+						}
+					}
+				}
+			}
+		}(content[i], content[i+1], content[i+2], ch)
 	}
+	c := 0
 	for i := 0; i < len(content)/3; i += 1 {
 		c += <-ch
 	}
 	fmt.Println(c)
-}
-
-func calc(l1, l2, l3 string, ch chan int) {
-	mar := false
-	c := 0
-	for _, k1 := range l1 {
-		for _, k2 := range l2 {
-			for _, k3 := range l3 {
-				if !mar && k1 == k2 && k2 == k3 {
-					mar = true
-					p := int(k1)
-					if p >= 97 {
-						c += p - 96
-					} else {
-						c += p - 38
-					}
-				}
-			}
-		}
-	}
-	ch <- c
 }
