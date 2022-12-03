48742f36bd4eec8c7928ec783b394db9f2558d55
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-03 11:15:38 +0100

    fast

--- 2022 (Go)/03/03.go
+++ 2022 (Go)/03/03.go
@@ -2,29 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	f, err := os.ReadFile("inp.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	f, _ := os.ReadFile("inp.txt")
 	content := strings.Split(strings.Trim(string(f), " \n"), "\n")
 	a := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	ch := make(chan int, len(content)/3)
 	for i := 0; i < len(content); i += 3 {
 		go func(l1, l2, l3 string, ch chan int) {
 			for _, k1 := range l1 {
-				for _, k2 := range l2 {
-					for _, k3 := range l3 {
-						if k1 == k2 && k2 == k3 {
-							ch <- strings.IndexRune(a, k1) + 1
-							return
-						}
-					}
+				if strings.ContainsRune(l2, k1) && strings.ContainsRune(l3, k1) {
+					ch <- strings.IndexRune(a, k1) + 1
+					return
 				}
 			}
 		}(content[i], content[i+1], content[i+2], ch)
