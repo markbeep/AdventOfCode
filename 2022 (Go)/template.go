7fd1c20088bb9122ac13d9664b7c30b1abac6fca
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-03 18:47:19 +0100

    bigger template

--- 2022 (Go)/template.go
+++ 2022 (Go)/template.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
+
+var hash = map[string]bool{}
 
 func main() {
 	f, err := os.ReadFile("inp.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var c int = 0
-	content := strings.Split(strings.Trim(string(f), " \n"), "\n")
+	cont := strings.Split(strings.Trim(string(f), " \n"), "\n")
+	c := 0
+	re := regexp.MustCompile(`(\w+)`)
+	var mat [][]string // group matches. mat[0][1] is the actual captured word
 
-	_ = content
+	for _, v := range cont {
+		mat = re.FindAllStringSubmatch(v, -1)
+	}
 
+	_ = cont
+	_ = mat
 	fmt.Println(c)
 }
