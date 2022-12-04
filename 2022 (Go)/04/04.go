21315909e383bf0d4b7402fd2956778bc72d6710
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-04 06:45:13 +0100

    day 4

--- 2022 (Go)/04/04.go
+++ 2022 (Go)/04/04.go
@@ -5,10 +5,9 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
-
-var hash = map[string]bool{}
 
 func main() {
 	f, err := os.ReadFile("inp.txt")
@@ -16,15 +15,21 @@ func main() {
 		log.Fatal(err)
 	}
 	cont := strings.Split(strings.Trim(string(f), " \n"), "\n")
-	c := 0
-	re := regexp.MustCompile(`(\w+)`)
-	var mat [][]string // group matches. mat[0][1] is the actual captured word
-
+	c1 := 0 // part 1
+	c2 := 0 // part 2
+	re := regexp.MustCompile(`(\d+)-(\d+),(\d+)-(\d+)`)
 	for _, v := range cont {
-		mat = re.FindAllStringSubmatch(v, -1)
+		m := re.FindAllStringSubmatch(v, -1)[0][1:]
+		k1, _ := strconv.Atoi(m[0])
+		k2, _ := strconv.Atoi(m[1])
+		k3, _ := strconv.Atoi(m[2])
+		k4, _ := strconv.Atoi(m[3])
+		if k1 <= k3 && k2 >= k4 || k3 <= k1 && k4 >= k2 {
+			c1++
+		}
+		if k1 <= k3 && k2 >= k3 || k3 <= k1 && k4 >= k1 {
+			c2++
+		}
 	}
-
-	_ = cont
-	_ = mat
-	fmt.Println(c)
+	fmt.Printf("Part 1: %d\nPart 2: %d\n", c1, c2)
 }
