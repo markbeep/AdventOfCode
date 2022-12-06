b8b22180453d261d74f6c0ccb8a0851c2d11004a
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-06 08:33:40 +0100

    clean

--- 2022 (Go)/06/06.go
+++ 2022 (Go)/06/06.go
@@ -11,32 +11,22 @@ func main() {
 	start := time.Now()
 	f, _ := os.ReadFile("inp.txt")
 	cont := strings.Trim(string(f), " \n")
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	// routes := 10
-	go ff(4, cont, ch1)
-	go ff(14, cont, ch2)
-	fmt.Printf("P1: %d\nP2: %d\n", <-ch1, <-ch2)
+	ch := make(chan int, 2)
+	go ff(4, cont, ch)
+	go ff(14, cont, ch)
+	fmt.Printf("P1: %d\nP2: %d\n", <-ch, <-ch)
 	fmt.Println("Took:", time.Since(start))
 }
 
 func ff(p int, cont string, ch chan int) {
 	t := make([]int, 26+int('a'))
 	c := 0
-	last := 0
 	for i, v := range cont {
-		b := t[int(v)]
-		if i-b <= p {
-			if b > last {
-				c = i - b - 1
-				last = b
-			} else {
-				c = 0
-			}
+		if t[int(v)] > c {
+			c = t[int(v)]
 		}
 		t[int(v)] = i
-		c++
-		if c == p {
+		if i-c == p {
 			ch <- i + 1
 			return
 		}
