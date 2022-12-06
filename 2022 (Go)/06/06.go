987fa9e7a1f14ae8e56083b2f04572a15643c747
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-06 08:22:58 +0100

    faster

--- 2022 (Go)/06/06.go
+++ 2022 (Go)/06/06.go
@@ -4,24 +4,39 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	f, _ := os.ReadFile("inp.txt")
 	cont := strings.Trim(string(f), " \n")
-	ch := make(chan int, 2)
-	go ff(4, cont, ch)
-	go ff(14, cont, ch)
-	fmt.Printf("P1: %d\nP2: %d\n", <-ch, <-ch)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	// routes := 10
+	go ff(4, cont, ch1)
+	go ff(14, cont, ch2)
+	fmt.Printf("P1: %d\nP2: %d\n", <-ch1, <-ch2)
+	fmt.Println("Took:", time.Since(start))
 }
 
 func ff(p int, cont string, ch chan int) {
-	for i := p; i < len(cont); i++ {
-		var hash = map[byte]bool{}
-		for j := 0; j < p; j++ {
-			hash[cont[i-j]] = true
+	t := make([]int, 26+int('a'))
+	c := 0
+	last := 0
+	for i, v := range cont {
+		b := t[int(v)]
+		if i-b <= p {
+			if b > last {
+				c = i - b - 1
+				last = b
+			} else {
+				c = 0
+			}
 		}
-		if len(hash) >= p {
+		t[int(v)] = i
+		c++
+		if c == p {
 			ch <- i + 1
 			return
 		}
