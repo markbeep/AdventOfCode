27dd95ea5b3d1cb0bc9c42b33caa884ebd1d49a2
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-08 12:39:53 +0100

    cleaned up

--- 2022 (Go)/08/08.go
+++ 2022 (Go)/08/08.go
@@ -7,14 +7,6 @@ import (
 	"time"
 )
 
-type tree struct {
-	l   int
-	r   int
-	t   int
-	b   int
-	vis bool
-}
-
 func main() {
 	start := time.Now()
 	f, _ := os.ReadFile("inp.txt")
@@ -48,18 +40,15 @@ func main() {
 	fmt.Println(c, m)
 	fmt.Println("Took", time.Since(start))
 }
-
-// returns left, right, top, bottom, isVisible
 func check(vis []string, x, y int) (int, bool) {
 	see := y*x*(y-len(vis)+1)*(x-len(vis[y])+1) == 0 // edges
 	dirs := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 	p := 1
-
 	for _, d := range dirs {
 		for r := range vis {
 			i := y + d[0]*(r+1)
 			j := x + d[1]*(r+1)
-			if (i >= 0 && i <= len(vis)-1 && j >= 0 && j <= len(vis)-1) && vis[i][j] >= vis[y][x] || i == 0 || i == len(vis)-1 || j == 0 || j == len(vis)-1 {
+			if i <= 0 || i >= len(vis)-1 || j <= 0 || j >= len(vis)-1 || vis[i][j] >= vis[y][x] {
 				p *= r + 1
 				see = see || vis[i][j] < vis[y][x]
 				break
