9415d0e5675530b8db276ddc4bfa7fbc0aa5452a
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-08 10:04:09 +0100

    cleaned up

--- 2022 (Go)/08/08.go
+++ 2022 (Go)/08/08.go
@@ -4,58 +4,50 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	f, err := os.ReadFile("inp.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	cont := strings.Split(strings.Trim(string(f), " \n"), "\n")
-	c := 0
-	trees := make([][]int, len(cont))
-	for i := range trees {
-		trees[i] = make([]int, len(cont[0]))
-		for j := range trees[i] {
-			val, _ := strconv.Atoi(string(cont[i][j]))
-			trees[i][j] = int(val)
-		}
-	}
-
-	m := 0
-	for i := range trees {
-		for j := range trees[i] {
-			if check(trees, i, j) {
-				c += 1
+	var c, m int
+	for i := range cont {
+		for j := range cont[i] {
+			l, r, t, b, see := check(cont, i, j)
+			if see {
+				c++
 			}
-			val := check2(trees, i, j)
-			if val > m {
-				m = val
+			if l*r*t*b > m {
+				m = l * r * t * b
 			}
 		}
 	}
 	fmt.Println(c, m)
+	fmt.Println("Took", time.Since(start))
 }
 
-func check2(vis [][]int, x, y int) int {
-	top := 0
-	bot := 0
-	l := 0
-	r := 0
-
-	// top down
+// returns left, right, top, bottom, isVisible
+func check(vis []string, x, y int) (int, int, int, int, bool) {
+	var l, r, b, t int
+	see := y*x*(y-len(vis)+1)*(x-len(vis[y])+1) == 0 // edges
+	// top
 	for i := y - 1; i >= 0; i-- {
 		if vis[i][x] >= vis[y][x] || i == 0 {
-			top = y - i
+			t = y - i
+			see = see || vis[i][x] < vis[y][x]
 			break
 		}
 	}
 	// bottom
 	for i := y + 1; i < len(vis); i++ {
 		if vis[i][x] >= vis[y][x] || i == len(vis)-1 {
-			bot = i - y
+			b = i - y
+			see = see || vis[i][x] < vis[y][x]
 			break
 		}
 	}
@@ -63,6 +55,7 @@ func check2(vis [][]int, x, y int) int {
 	for i := x - 1; i >= 0; i-- {
 		if vis[y][i] >= vis[y][x] || i == 0 {
 			l = x - i
+			see = see || vis[y][i] < vis[y][x]
 			break
 		}
 	}
@@ -70,56 +63,9 @@ func check2(vis [][]int, x, y int) int {
 	for i := x + 1; i < len(vis[0]); i++ {
 		if vis[y][i] >= vis[y][x] || i == len(vis[0])-1 {
 			r = i - x
+			see = see || vis[y][i] < vis[y][x]
 			break
 		}
 	}
-	return l * r * top * bot
-}
-
-func check(vis [][]int, x, y int) bool {
-	if x == 0 || x == len(vis)-1 || y == 0 || y == len(vis[0])-1 {
-		return true
-	}
-
-	see := false
-	// top down
-	for i := 0; i < y; i++ {
-		if vis[i][x] >= vis[y][x] {
-			see = false || see
-			break
-		}
-		if i == y-1 {
-			see = true
-		}
-	}
-	// bottom
-	for i := len(vis) - 1; i > y; i-- {
-		if vis[i][x] >= vis[y][x] {
-			see = false || see
-			break
-		}
-		if i == y+1 {
-			see = true
-		}
-	}
-	// left
-	for i := 0; i < x; i++ {
-		if vis[y][i] >= vis[y][x] {
-			see = false || see
-			break
-		}
-		if i == x-1 {
-			see = true
-		}
-	}
-	for i := len(vis[0]) - 1; i > x; i-- {
-		if vis[y][i] >= vis[y][x] {
-			see = false || see
-			break
-		}
-		if i == x+1 {
-			see = true
-		}
-	}
-	return see
+	return l, r, t, b, see
 }
