55dd892c076b1b5c8462ac143917e01f6edc1ad9
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-08 12:27:23 +0100

    shorter

--- 2022 (Go)/08/08.go
+++ 2022 (Go)/08/08.go
@@ -25,12 +25,12 @@ func main() {
 		go func(i int, cch, mch chan int) {
 			var c, m int
 			for j := range cont[i] {
-				l, r, t, b, vis := check(cont, i, j)
+				val, vis := check(cont, i, j)
 				if vis {
 					c++
 				}
-				if l*r*t*b > m {
-					m = l * r * t * b
+				if val > m {
+					m = val
 				}
 			}
 			cch <- c
@@ -50,40 +50,21 @@ func main() {
 }
 
 // returns left, right, top, bottom, isVisible
-func check(vis []string, x, y int) (int, int, int, int, bool) {
-	var l, r, b, t int
+func check(vis []string, x, y int) (int, bool) {
 	see := y*x*(y-len(vis)+1)*(x-len(vis[y])+1) == 0 // edges
-	// top
-	for i := y - 1; i >= 0; i-- {
-		if vis[i][x] >= vis[y][x] || i == 0 {
-			t = y - i
-			see = see || vis[i][x] < vis[y][x]
-			break
+	dirs := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	p := 1
+
+	for _, d := range dirs {
+		for r := range vis {
+			i := y + d[0]*(r+1)
+			j := x + d[1]*(r+1)
+			if (i >= 0 && i <= len(vis)-1 && j >= 0 && j <= len(vis)-1) && vis[i][j] >= vis[y][x] || i == 0 || i == len(vis)-1 || j == 0 || j == len(vis)-1 {
+				p *= r + 1
+				see = see || vis[i][j] < vis[y][x]
+				break
+			}
 		}
 	}
-	// bottom
-	for i := y + 1; i < len(vis); i++ {
-		if vis[i][x] >= vis[y][x] || i == len(vis)-1 {
-			b = i - y
-			see = see || vis[i][x] < vis[y][x]
-			break
-		}
-	}
-	// left
-	for i := x - 1; i >= 0; i-- {
-		if vis[y][i] >= vis[y][x] || i == 0 {
-			l = x - i
-			see = see || vis[y][i] < vis[y][x]
-			break
-		}
-	}
-	// right
-	for i := x + 1; i < len(vis[0]); i++ {
-		if vis[y][i] >= vis[y][x] || i == len(vis[0])-1 {
-			r = i - x
-			see = see || vis[y][i] < vis[y][x]
-			break
-		}
-	}
-	return l, r, t, b, see
+	return p, see
 }
