3348d5271dd244ffd3ac081e4cc99c774e3d8ad2
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-10 09:03:33 +0100

    kek

--- 2022 (Go)/10/10.go
+++ 2022 (Go)/10/10.go
@@ -10,11 +10,33 @@ func main() {
 	c := 0
 	hash := map[string]bool{}
 
-	for _, v := range f {
-		fmt.Sscanf(v, "")
+	queue := make([]int, 1)
+	x := 1
+	i := 0
+	for ; len(queue) > 0; i++ {
+		po := queue[0]
+		x += po
+		queue = queue[1:]
+		if len(f) > 0 {
+			v := f[0]
+			f = f[1:]
+			if v == "noop" {
+				queue = append(queue, 0)
+			} else {
+				var k int
+				fmt.Sscanf(v, "addx %d", &k)
+				queue = append(queue, 0)
+				queue = append(queue, k)
+			}
+		}
+		fmt.Println(queue)
+		if (i-19)%40 == 0 {
+			fmt.Println(i, x)
+			c += (i + 1) * x
+		}
 
 	}
 	_ = f
 	_ = hash
-	fmt.Println(c)
+	fmt.Println(x, c)
 }
