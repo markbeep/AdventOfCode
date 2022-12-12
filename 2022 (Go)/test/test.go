c68b14f8702fbdcb3f939304b9bd7c211e6cee20
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-12 09:07:35 +0100

    generic bfs algo

--- 2022 (Go)/test/test.go
+++ 2022 (Go)/test/test.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"aoc/util"
 	"aoc/util/node"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	n := node.Create(5)
-	fmt.Println(n)
+	f := util.ReadS("inp.txt", "\n")
+	var s, e *node.Node[rune]
+	ns := util.Array[*node.Node[rune]](len(f), len(f[0]))
+	for i, a := range f {
+		for j, b := range a {
+			ns[i][j] = node.Create(b)
+			if b == 'E' {
+				e = ns[i][j]
+				f[i] = strings.Replace(f[i], "E", "z", 1)
+			} else if b == 'S' {
+				s = ns[i][j]
+				f[i] = strings.Replace(f[i], "S", "a", 1)
+			}
+		}
+	}
+	for i, a := range f {
+		for j, b := range a {
+			if valid(f, j+1, i, b) {
+				ns[i][j].Sub = append(ns[i][j].Sub, ns[i][j+1])
+			}
+			if valid(f, j, i+1, b) {
+				ns[i][j].Sub = append(ns[i][j].Sub, ns[i+1][j])
+			}
+			if valid(f, j-1, i, b) {
+				ns[i][j].Sub = append(ns[i][j].Sub, ns[i][j-1])
+			}
+			if valid(f, j, i-1, b) {
+				ns[i][j].Sub = append(ns[i][j].Sub, ns[i-1][j])
+			}
+		}
+	}
+	fmt.Println(node.Bfs([]*node.Node[rune]{s}, map[*node.Node[rune]]bool{}, make(map[*node.Node[rune]]int))[e])
+
+}
+
+func valid(f []string, x, y int, cur rune) bool {
+	return y >= 0 && y < len(f) && x >= 0 && x < len(f[y]) && int(f[y][x])-int(cur) <= 1
 }
--- 2022 (Go)/util/node/node.go
+++ 2022 (Go)/util/node/node.go
@@ -1,18 +1,33 @@
 package node
 
 type Node[T any] struct {
-	val T
-	par *Node[T]
-	sub []*Node[T]
+	Val T
+	Par *Node[T]
+	Sub []*Node[T]
 }
 
 func Create[T any](val T) *Node[T] {
-	return &Node[T]{val: val, sub: []*Node[T]{}}
+	return &Node[T]{Val: val, Sub: []*Node[T]{}}
 }
 
 func (n *Node[T]) Add(val T) *Node[T] {
-	n.sub = append(n.sub, Create(val))
-	c := &Node[T]{val: val, sub: []*Node[T]{}}
-	c.par = n
+	n.Sub = append(n.Sub, Create(val))
+	c := &Node[T]{Val: val, Sub: []*Node[T]{}}
+	c.Par = n
 	return c
+}
+
+func Bfs[T comparable](q []*Node[T], vis map[*Node[T]]bool, dist map[*Node[T]]int) map[*Node[T]]int {
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+		for _, c := range p.Sub {
+			if !vis[c] {
+				vis[c] = true
+				dist[c] = dist[p] + 1
+				q = append(q, c)
+			}
+		}
+	}
+	return dist
 }
