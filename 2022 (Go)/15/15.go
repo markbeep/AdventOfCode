08d1be430ee5be3fc34d7682eb8cca949145c391
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-15 12:53:26 +0100

    parallel day 15

--- 2022 (Go)/15/15.go
+++ 2022 (Go)/15/15.go
@@ -4,47 +4,66 @@ import (
 	"aoc/util"
 	"aoc/util/ints"
 	"fmt"
+	"runtime"
+	"sync"
 )
 
 type nrange struct {
 	from, to int
 }
+type sens struct {
+	sx, sy, bx, by, dist int
+}
 
 func main() {
 	f := util.ReadS("inp.txt", "\n")
-	c := 0
-
-	for i := 0; i < 20; i++ {
-	}
-	for _, v := range single(2000000, f) {
-		c += v.to - v.from
-	}
-	fmt.Println("Part 1:", c)
-
-}
-
-func single(line int, f []string) []nrange {
-	xranges := []nrange{}
+	P1, P2 := 2000000, 4000000
+	sensors := []sens{} // store all sensors and the beacon location
 	for _, v := range f {
 		var sx, sy, bx, by int
 		fmt.Sscanf(v, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sx, &sy, &bx, &by)
-		dist := ints.Abs(sx-bx) + ints.Abs(sy-by)
-		distToLine := ints.Abs(sy - line)
-		if distToLine > dist {
-			continue // ignore sensors too far away
+		sensors = append(sensors, sens{sx, sy, bx, by, ints.Abs(sx-bx) + ints.Abs(sy-by)})
+	}
+	c := 0
+	for _, v := range single(P1, sensors) {
+		c += v.to - v.from
+	}
+	fmt.Print("Part 1: ", c)
+	NUM_ROUTS := ints.Min(runtime.GOMAXPROCS(0), runtime.NumCPU())
+	SPLIT := P2 / NUM_ROUTS
+	wg := new(sync.WaitGroup)
+	for i := 0; i < NUM_ROUTS; i++ {
+		wg.Add(1)
+		go func(fr, to int, wg *sync.WaitGroup) {
+			for i := fr; i < ints.Min(P2, to); i++ {
+				if rng := single(i, sensors); len(rng) == 2 {
+					fmt.Printf(" | Part 2: %d (%d, %d)\n", i+(ints.Min(rng[0].to, rng[1].to)+1)*4000000, i, ints.Min(rng[0].to, rng[1].to)+1)
+					break
+				}
+			}
+			wg.Done()
+		}(i*SPLIT, (i+1)*SPLIT, wg)
+	}
+	wg.Wait()
+
+}
+
+func single(line int, sensors []sens) []nrange {
+	xranges := []nrange{}
+	for _, v := range sensors {
+		if distToLine := ints.Abs(v.sy - line); distToLine <= v.dist {
+			size := ints.Abs(distToLine - v.dist)
+			xranges = combine(xranges, nrange{from: v.sx - size, to: v.sx + size})
 		}
-		size := ints.Abs(distToLine - dist)
-		xranges = combine(xranges, nrange{from: sx - size, to: sx + size})
 	}
 	return xranges
 }
 
 // adds a new range and handles intersections
 func combine(rngs []nrange, r nrange) []nrange {
-	nw := []nrange{}
+	nw := make([]nrange, 0, 2)
 	for _, v := range rngs {
-		in := intersect(v, r)
-		if len(in) == 2 { // v is disjunct
+		if in := intersect(v, r); len(in) == 2 { // v is disjunct
 			nw = append(nw, v)
 		} else {
 			r = in[0]
@@ -57,7 +76,7 @@ func combine(rngs []nrange, r nrange) []nrange {
 // returns both or an intersected range
 func intersect(r1, r2 nrange) []nrange {
 	// dont intersect
-	if r1.to < r2.from || r2.to < r1.from {
+	if r1.to+1 < r2.from || r2.to+1 < r1.from {
 		return []nrange{r1, r2}
 	}
 	// contained
@@ -68,7 +87,5 @@ func intersect(r1, r2 nrange) []nrange {
 		return []nrange{r1}
 	}
 	// intersect (take min/max of both ranges)
-	mx := ints.Max(r1.to, r2.to)
-	mn := ints.Min(r1.from, r2.from)
-	return []nrange{{from: mn, to: mx}}
+	return []nrange{{from: ints.Min(r1.from, r2.from), to: ints.Max(r1.to, r2.to)}}
 }
--- 2022 (Go)/15/15_test.go
+++ 2022 (Go)/15/15_test.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"aoc/util"
+	"aoc/util/ints"
+	"fmt"
 	"testing"
 )
 
 func BenchmarkRun(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		main()
+	}
+}
+
+func BenchmarkSingle(b *testing.B) {
+	f := util.ReadS("inp.txt", "\n")
+	sensors := []sens{} // store all sensors and the beacon location
+	for _, v := range f {
+		var sx, sy, bx, by int
+		fmt.Sscanf(v, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sx, &sy, &bx, &by)
+		sensors = append(sensors, sens{sx, sy, bx, by, ints.Abs(sx-bx) + ints.Abs(sy-by)})
+	}
+	for i := 0; i < b.N; i++ {
+		single(10, sensors)
+	}
+}
+
+func BenchmarkCombine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		combine([]nrange{{-123123123, 5124123}, {5124523, 55124123}}, nrange{5124023, 51124123})
+	}
+}
+
+func BenchmarkIntersect(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		intersect(nrange{-123123123, 5124123}, nrange{5124023, 51124123})
 	}
 }
