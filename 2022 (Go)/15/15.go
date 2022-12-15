8205aa4d33cf471b6e72e49a49253eda97dda09a
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-12-15 09:56:50 +0100

    part 1

---
+++ 2022 (Go)/15/15.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"aoc/util"
+	"aoc/util/ints"
+	"fmt"
+)
+
+type nrange struct {
+	from, to int
+}
+
+func main() {
+	f := util.ReadS("inp.txt", "\n")
+	c := 0
+
+	line := 2000000
+	// inclusive ranges
+	xranges := []nrange{}
+	// yranges := []nrange{}
+
+	for _, v := range f {
+		var sx, sy, bx, by int
+		fmt.Sscanf(v, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sx, &sy, &bx, &by)
+		dist := ints.Abs(sx-bx) + ints.Abs(sy-by)
+		distToLine := ints.Abs(sy - line)
+		if distToLine > dist {
+			continue // ignore sensors too far away
+		}
+		size := ints.Abs(distToLine - dist)
+		xranges = combine(xranges, nrange{from: sx - size, to: sx + size})
+	}
+	for _, v := range xranges {
+		c += v.to - v.from
+	}
+
+	fmt.Println(xranges)
+
+	_ = f
+	fmt.Println(c)
+}
+
+// adds a new range and handles intersections
+func combine(rngs []nrange, r nrange) []nrange {
+	nw := []nrange{}
+	for _, v := range rngs {
+		in := intersect(v, r)
+		if len(in) == 2 { // v is disjunct
+			nw = append(nw, v)
+		} else {
+			r = in[0]
+		}
+	}
+	nw = append(nw, r)
+	return nw
+}
+
+// returns both or an intersected range
+func intersect(r1, r2 nrange) []nrange {
+	// dont intersect
+	if r1.to < r2.from || r2.to < r1.from {
+		return []nrange{r1, r2}
+	}
+	// contained
+	if r1.from >= r2.from && r1.to <= r2.to {
+		return []nrange{r2}
+	}
+	if r2.from >= r1.from && r2.to <= r1.to {
+		return []nrange{r1}
+	}
+	// intersect (take min/max of both ranges)
+	mx := ints.Max(r1.to, r2.to)
+	mn := ints.Min(r1.from, r2.from)
+	return []nrange{{from: mn, to: mx}}
+}
--- 2022 (Go)/template.go
+++ 2022 (Go)/template.go
@@ -8,13 +8,11 @@ import (
 func main() {
 	f := util.ReadS("inp.txt", "\n")
 	c := 0
-	hash := map[string]bool{}
 
 	for _, v := range f {
 		fmt.Sscanf(v, "")
 
 	}
 	_ = f
-	_ = hash
 	fmt.Println(c)
 }
