8458a6957f2dcdd6e20c63a6a553a8d0e7eafadf
Mark <mrkswrn@gmail.com>
2023-12-03 18:26:37 +0100

    more clean up

--- 2023-Go/03/03.go
+++ 2023-Go/03/03.go
@@ -31,22 +31,31 @@ func main() {
 	}
 }
 
+type pos struct {
+	x, y int
+}
+
 func isInt(input rune) bool {
 	return input >= '0' && input <= '9'
 }
 
-func touches(y, x int, f []string) bool {
+func touches(y, x int, f []string) (bool, *pos) {
+	touch := false
+	var star *pos = nil
 	for yi := y - 1; yi <= y+1; yi++ {
 		for xi := x - 1; xi <= x+1; xi++ {
 			if yi < 0 || yi >= len(f) || xi < 0 || xi >= len(f[0]) {
 				continue
 			}
 			if !isInt(rune(f[yi][xi])) && f[yi][xi] != '.' {
-				return true
+				touch = true
+			}
+			if f[yi][xi] == '*' {
+				star = &pos{x: xi, y: yi}
 			}
 		}
 	}
-	return false
+	return touch, star
 }
 
 func part1(input string) int {
@@ -59,9 +68,8 @@ func part1(input string) int {
 		for x, char := range line {
 			if isInt(char) {
 				current += string(char)
-				if touches(y, x, f) {
-					touch = true
-				}
+				touchesSymbol, _ := touches(y, x, f)
+				touch = touchesSymbol || touch
 			} else {
 				if len(current) > 0 && touch {
 					c += ints.SInt(current)
@@ -78,21 +86,19 @@ func part1(input string) int {
 	return c
 }
 
-type pos struct {
-	x, y int
-}
-
 func part2(input string) int {
 	f := strings.Split(input, "\n")
 	gear := map[pos]int{}
 	c := 0
 
-	combineCurrent := func(current string, star pos) {
-		prev, ok := gear[star]
-		if ok {
-			c += prev * ints.SInt(current)
-		} else {
-			gear[star] = ints.SInt(current)
+	combineCurrent := func(current string, star *pos) {
+		if len(current) > 0 && star != nil {
+			prev, ok := gear[*star]
+			if ok {
+				c += prev * ints.SInt(current)
+			} else {
+				gear[*star] = ints.SInt(current)
+			}
 		}
 	}
 
@@ -102,27 +108,17 @@ func part2(input string) int {
 		for x, char := range line {
 			if isInt(char) {
 				current += string(char)
-				for yi := y - 1; yi <= y+1; yi++ {
-					for xi := x - 1; xi <= x+1; xi++ {
-						if yi < 0 || yi >= len(f) || xi < 0 || xi >= len(f[0]) {
-							continue
-						}
-						if f[yi][xi] == '*' {
-							star = &pos{x: xi, y: yi}
-						}
-					}
+				_, newStar := touches(y, x, f)
+				if newStar != nil {
+					star = newStar
 				}
 			} else {
-				if len(current) > 0 && star != nil {
-					combineCurrent(current, *star)
-				}
+				combineCurrent(current, star)
 				star = nil
 				current = ""
 			}
 		}
-		if len(current) > 0 && star != nil {
-			combineCurrent(current, *star)
-		}
+		combineCurrent(current, star)
 	}
 
 	return c
