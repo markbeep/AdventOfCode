c70ef96b99e57abfbccc4f0f60978e26b9321c12
Mark <mrkswrn@gmail.com>
2023-12-03 18:15:12 +0100

    clean up

--- 2023-Go/03/03.go
+++ 2023-Go/03/03.go
@@ -78,79 +78,50 @@ func part1(input string) int {
 	return c
 }
 
-func getNumStart(y, x int, f []string) int {
-	for {
-		x--
-		if x < 0 {
-			x = 0
-			break
-		}
-		if !isInt(rune(f[y][x])) {
-			x += 1
-			break
-		}
-	}
-	return x
-}
-
-func getNum(y, x int, f []string) int {
-	current := ""
-	// find start
-	for {
-		x--
-		if x < 0 {
-			x = 0
-			break
-		}
-		if !isInt(rune(f[y][x])) {
-			x += 1
-			break
-		}
-	}
-	// get whole int
-	for {
-		if x >= len(f[0]) {
-			break
-		}
-		if !isInt(rune(f[y][x])) {
-			break
-		}
-		current += string(f[y][x])
-		x++
-	}
-	return ints.SInt(current)
+type pos struct {
+	x, y int
 }
 
 func part2(input string) int {
 	f := strings.Split(input, "\n")
-	var added = map[string]bool{}
+	gear := map[pos]int{}
 	c := 0
 
+	combineCurrent := func(current string, star pos) {
+		prev, ok := gear[star]
+		if ok {
+			c += prev * ints.SInt(current)
+		} else {
+			gear[star] = ints.SInt(current)
+		}
+	}
+
 	for y, line := range f {
+		current := ""
+		var star *pos
 		for x, char := range line {
-			if char == '*' {
-				numbers := []int{}
-				// go around star and check for numbers
+			if isInt(char) {
+				current += string(char)
 				for yi := y - 1; yi <= y+1; yi++ {
 					for xi := x - 1; xi <= x+1; xi++ {
 						if yi < 0 || yi >= len(f) || xi < 0 || xi >= len(f[0]) {
 							continue
 						}
-						if isInt(rune(f[yi][xi])) {
-							num := getNum(yi, xi, f)
-							start := getNumStart(yi, xi, f)
-							if !added[fmt.Sprintf("%d,%d", yi, start)] {
-								numbers = append(numbers, num)
-								added[fmt.Sprintf("%d,%d", yi, start)] = true
-							}
+						if f[yi][xi] == '*' {
+							star = &pos{x: xi, y: yi}
 						}
 					}
 				}
-				// multiply
-				if len(numbers) == 2 {
-					c += numbers[0] * numbers[1]
+			} else {
+				if len(current) > 0 && star != nil {
+					combineCurrent(current, *star)
 				}
+				star = nil
+				current = ""
 			}
+		}
+		if len(current) > 0 && star != nil {
+			combineCurrent(current, *star)
 		}
 	}
 
