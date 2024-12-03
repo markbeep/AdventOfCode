036a23cadddee91cf6a05071d483fb151e77ff5f
Mark <mrkswrn@gmail.com>
2024-12-03 08:44:13 +0100

    day 3

--- 2024-Rust/src/days/day03.rs
+++ 2024-Rust/src/days/day03.rs
@@ -1,5 +1,22 @@
+use regex::Regex;
+use std::fs::read_to_string;
+
 use crate::Solution;
 
 pub fn solve() -> Solution {
-    ("".to_string(), "".to_string())
+    let f = read_to_string("input/03.txt").unwrap();
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut p: (i64, i64) = (0, 0);
+    for cap in re.captures_iter(&f) {
+        let [a, b] = cap.extract().1.map(|x| x.parse::<i64>().unwrap());
+        p.0 += a * b;
+        let ofs = cap.get(0).unwrap().start();
+        let split = f.split_at(ofs).0;
+        match (split.rfind("do()"), split.rfind("don't()")) {
+            (None, None) | (Some(_), None) => p.1 += a * b,
+            (Some(x), Some(y)) if x > y => p.1 += a * b,
+            _ => {}
+        };
+    }
+    (p.0.to_string(), p.1.to_string())
 }
--- 2024-Rust/time.sh
+++ 2024-Rust/time.sh
@@ -1,1 +1,1 @@
-cargo build --release && hyperfine -m 10000 -N --warmup 10 "target/release/aoc $1"
+cargo build --release && hyperfine -N --warmup 10 "target/release/aoc $1"
