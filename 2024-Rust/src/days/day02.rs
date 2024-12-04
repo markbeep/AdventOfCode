2a1b0f7d3b477e08eade6316493d4f34589c279d
Markbeep <mrkswrn@gmail.com>
2024-12-04 09:22:15 +0100

    wrong

--- 2024-Rust/src/days/day02.rs
+++ 2024-Rust/src/days/day02.rs
@@ -1,5 +1,35 @@
+use std::fs::read_to_string;
+
 use crate::Solution;
 
 pub fn solve() -> Solution {
-    ("".to_string(), "".to_string())
+    let f = read_to_string("input/02.txt").unwrap();
+    let reports = f.trim().split("\n").map(|x| {
+        x.split(" ")
+            .map(|n| n.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>()
+    });
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for report in reports {
+        if is_safe(report.to_owned()) {
+            p1 += 1;
+            p2 += 1;
+            continue;
+        }
+        for i in 0..report.len() {
+            let mut rep = report.clone();
+            rep.remove(i);
+            if is_safe(rep) {
+                p2 += 1;
+                break;
+            }
+        }
+    }
+    (p1.to_string(), p2.to_string())
+}
+
+fn is_safe(x: Vec<i64>) -> bool {
+    x.is_sorted_by(|a, b| (-3..=-1).contains(&(b - a)))
+        || x.is_sorted_by(|a, b| (1..=3).contains(&(b - a)))
 }
--- 2024-Rust/src/days/day04.rs
+++ 2024-Rust/src/days/day04.rs
@@ -1,5 +1,39 @@
+use std::fs::read_to_string;
+
 use crate::Solution;
 
 pub fn solve() -> Solution {
-    ("".to_string(), "".to_string())
+    let f = read_to_string("input/04.txt").unwrap();
+    let t = f.trim().to_string();
+    let arr: Vec<Vec<char>> = t
+        .split("\n")
+        .map(|x| x.chars().collect::<Vec<char>>())
+        .collect();
+    let mut combinations = t.to_owned();
+    let mut diag_down = String::new();
+    let mut diag_up = String::new();
+    let mut down = String::new();
+
+    for x in 0..arr[0].len() {
+        for y in 0..arr.len() {
+            down.push_str(&arr[y][x].to_string());
+            let xr = x + y;
+            let yd = arr.len() - y - 1;
+            if xr < arr[0].len() && y + 1 < arr.len() {
+                diag_down.push_str(&arr[y][xr].to_string());
+            }
+            if xr < arr[0].len() && yd > 0 {
+                diag_up.push_str(&arr[yd][xr].to_string());
+            }
+        }
+    }
+    combinations.push_str(&diag_down);
+    combinations.push_str(&diag_up);
+    combinations.push_str(&down);
+    let backwards = combinations.chars().rev().collect::<String>();
+    combinations.push_str(&backwards);
+    let p1 = combinations.match_indices("XMAS").count();
+
+    let p1 = String::from("XXMAS").match_indices("XMAS").count();
+    (p1.to_string(), "".to_string())
 }
