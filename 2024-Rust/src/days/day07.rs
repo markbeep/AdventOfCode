db601491c282699ed0b82a602cb5a218ea3d702b
Markbeep <mrkswrn@gmail.com>
2024-12-07 12:05:54 +0100

    p2

--- 2024-Rust/src/days/day07.rs
+++ 2024-Rust/src/days/day07.rs
@@ -20,15 +20,26 @@ pub fn solve() -> Solution {
         .collect();
     let p1: usize = rows
         .iter()
-        .filter(|(sum, nums)| rec(*sum, 0, nums, 0))
+        .filter(|(sum, nums)| rec(*sum, 0, nums, 0, false))
         .map(|(sum, _)| sum)
         .sum();
-    (p1.to_string(), "".to_string())
+    let p2: usize = rows
+        .iter()
+        .filter(|(sum, nums)| rec(*sum, 0, nums, 0, true))
+        .map(|(sum, _)| sum)
+        .sum();
+    (p1.to_string(), p2.to_string())
 }
 
-fn rec(sum: usize, cur: usize, nums: &Vec<usize>, i: usize) -> bool {
+fn concat(a: usize, b: usize) -> usize {
+    a * (10 as usize).pow(1 + b.ilog10()) + b
+}
+
+fn rec(sum: usize, cur: usize, nums: &Vec<usize>, i: usize, c: bool) -> bool {
     if i == nums.len() {
         return sum == cur;
     }
-    rec(sum, cur + nums[i], nums, i + 1) || rec(sum, cur * nums[i], nums, i + 1)
+    rec(sum, cur + nums[i], nums, i + 1, c)
+        || rec(sum, cur * nums[i], nums, i + 1, c)
+        || c && rec(sum, concat(cur, nums[i]), nums, i + 1, c)
 }
