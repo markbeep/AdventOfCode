9fac6d072c6691dd428948cbab7566180450f0db
Mark <mrkswrn@gmail.com>
2024-12-11 09:46:13 +0100

    p2

--- 2024-Rust/src/days/day11.rs
+++ 2024-Rust/src/days/day11.rs
@@ -1,32 +1,57 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 use crate::Solution;
 
 pub fn solve() -> Solution {
     let f = read_to_string("input/11.txt").unwrap();
-    let mut nums: Vec<u64> = f
+    let nums: Vec<u64> = f
         .trim()
         .split(" ")
         .map(|x| x.parse::<u64>().unwrap())
         .collect();
 
-    for i in 0..25 {
-        let mut cpy = vec![];
-        for n in nums {
+    let mut counts: HashMap<u64, u64> = HashMap::new();
+    nums.iter().for_each(|x| {
+        counts.insert(*x, 1);
+    });
+    let mut p1 = 0;
+    let mut p2 = 0;
+    for i in 0..75 {
+        let mut cpy = HashMap::new();
+        for (k, v) in counts.iter() {
+            let n = *k;
             if n == 0 {
-                cpy.push(1);
+                match cpy.get_mut(&1) {
+                    Some(x) => *x += *v,
+                    None => drop(cpy.insert(1, *v)),
+                }
             } else if n.ilog10() % 2 == 1 {
                 let log = 10u64.pow(n.ilog10() / 2 + 1);
                 let left = n / log;
                 let right = n % log;
-                cpy.push(left);
-                cpy.push(right);
+                match cpy.get_mut(&left) {
+                    Some(x) => *x += *v,
+                    None => drop(cpy.insert(left, *v)),
+                }
+                match cpy.get_mut(&right) {
+                    Some(x) => *x += *v,
+                    None => drop(cpy.insert(right, *v)),
+                }
             } else {
-                cpy.push(n * 2024);
+                match cpy.get_mut(&(n * 2024)) {
+                    Some(x) => *x += *v,
+                    None => drop(cpy.insert(n * 2024, *v)),
+                }
             }
         }
-        nums = cpy;
+        counts = cpy;
+        if i == 24 {
+            p1 = counts.values().sum();
+        }
+        if i == 74 {
+            p2 = counts.values().sum();
+        }
     }
 
-    (nums.len().to_string(), "".to_string())
+    (p1.to_string(), p2.to_string())
 }
