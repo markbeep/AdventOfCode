48d943ad2840405e69e7741390f5a9b3291ec702
Markbeep <mrkswrn@gmail.com>
2025-12-01 08:01:16 +0000

    zig 1

---
+++ 2025-Zig/src/days/01.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const cwd = std.fs.cwd();
+    const buf = try cwd.readFileAlloc("days/01.txt", alloc, .unlimited);
+    defer alloc.free(buf);
+
+    try p1(buf);
+    try p2(buf);
+}
+
+fn p1(buf: []u8) !void {
+    var pw: u32 = 0;
+    var c: i32 = 50;
+    var split_iterator = std.mem.splitScalar(u8, buf, '\n');
+    while (split_iterator.next()) |line| {
+        if (line.len == 0) continue;
+        const move = try std.fmt.parseInt(i32, line[1..], 10);
+        c = @mod(if (line[0] == 'L') c - move else c + move, 100);
+        if (c < 0) c += 100;
+        if (c == 0) pw += 1;
+    }
+    std.debug.print("p1: {d}\n", .{pw});
+}
+
+fn p2(buf: []u8) !void {
+    var pw: i32 = 0;
+    var c: i32 = 50;
+    var split_iterator = std.mem.splitScalar(u8, buf, '\n');
+    while (split_iterator.next()) |line| {
+        if (line.len == 0) continue;
+        const move = try std.fmt.parseInt(i32, line[1..], 10);
+        if (line[0] == 'L') {
+            if (move >= c) {
+                if (c != 0) pw += 1;
+                pw += @divFloor(move - c, 100);
+            }
+            c = @mod(c - move, 100);
+            if (c < 0) c += 100;
+        } else {
+            if (move >= 100 - c) {
+                pw += 1 + @divFloor(move - (100 - c), 100);
+            }
+            c = @mod(c + move, 100);
+        }
+    }
+    std.debug.print("p2: {d}\n", .{pw});
+}
---
+++ 2025-Zig/src/main.zig
@@ -0,0 +1,6 @@
+const _2025_Zig = @import("_2025_Zig");
+const _01 = @import("days/01.zig");
+
+pub fn main() !void {
+    try _01.main();
+}
